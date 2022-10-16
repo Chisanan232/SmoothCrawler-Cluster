da23c3897e1bef3e99c06583ba49b50e48edd431
“Chisanan232” <chi10211201@cycu.org.tw>
2022-10-16 17:21:41 +0800

    [Breaking Change] (code) 3 major changes:
    1. Rename function to '_convert_to_readable_object'.
    
    2. Modify the implementation of '_convert_to_readable_object'.
    
    3. Add TODO lists.

--- smoothcrawler_cluster/_utils/converter.py
+++ smoothcrawler_cluster/_utils/converter.py
@@ -1,8 +1,11 @@
 from abc import ABCMeta, abstractmethod
-from typing import Dict, Any
+from typing import Dict, Any, TypeVar, Generic
 import json
 
-from ..model.metadata import State, Task, Heartbeat
+from ..model.metadata import _BaseMetaData, State, Task, Heartbeat
+
+
+_BaseMetaDataType = TypeVar("_BaseMetaDataType", bound=_BaseMetaData)
 
 
 class BaseConverter(metaclass=ABCMeta):
@@ -28,24 +31,31 @@ class JsonStrConverter(BaseConverter):
         return _parsed_data
 
     def state_to_str(self, state: State) -> str:
-        return self.serialize(data=self._convert_to_str(obj=state))
+        # TODO: Move it to the super-class to be a template function
+        return self.serialize(data=self._convert_to_readable_object(obj=state))
 
     def task_to_str(self, task: Task) -> str:
-        return self.serialize(data=self._convert_to_str(obj=task))
+        # TODO: Move it to the super-class to be a template function
+        return self.serialize(data=self._convert_to_readable_object(obj=task))
 
     def heartbeat_to_str(self, heartbeat: Heartbeat) -> str:
-        return self.serialize(data=self._convert_to_str(obj=heartbeat))
+        # TODO: Move it to the super-class to be a template function
+        return self.serialize(data=self._convert_to_readable_object(obj=heartbeat))
 
-    def _convert_to_str(self, obj: Any) -> str:
-        return str(obj)
+    def _convert_to_readable_object(self, obj: Generic[_BaseMetaDataType]) -> Any:
+        # TODO:
+        #  1. Set it to be a abstract method
+        #  2. Rename the function name to be more clear
+        return obj.to_readable_object()
 
     def str_to_state(self, data: str) -> State:
-        # TODO: Maybe it could develop a package like mapstruct in kotlin.
+        # TODO: Move it to the super-class to be a template function
         _parsed_data: Dict[str, Any] = self.deserialize(data=data)
         _state_with_value = self._convert_to_state(state=State(), data=_parsed_data)
         return _state_with_value
 
     def _convert_to_state(self, state: State, data: Any) -> State:
+        # TODO: Maybe it could develop a package like mapstruct in kotlin.
         data: Dict[str, Any] = data
         state.role = data.get("role")
         state.total_crawler = data.get("total_crawler")
@@ -61,24 +71,26 @@ class JsonStrConverter(BaseConverter):
         return state
 
     def str_to_task(self, data: str) -> Task:
-        # TODO: Maybe it could develop a package like mapstruct in kotlin.
+        # TODO: Move it to the super-class to be a template function
         _parsed_data: Dict[str, Any] = self.deserialize(data=data)
         _task_with_value = self._convert_to_task(task=Task(), data=_parsed_data)
         return _task_with_value
 
     def _convert_to_task(self, task: Task, data: Any) -> Task:
+        # TODO: Maybe it could develop a package like mapstruct in kotlin.
         data: Dict[str, Any] = data
         task.task_content = data.get("task_content")
         task.task_result = data.get("task_result")
         return task
 
     def str_to_heartbeat(self, data: str) -> Heartbeat:
-        # TODO: Maybe it could develop a package like mapstruct in kotlin.
+        # TODO: Move it to the super-class to be a template function
         _parsed_data: Dict[str, Any] = self.deserialize(data=data)
         _heartbeat_with_value = self._convert_to_heartbeat(heartbeat=Heartbeat(), data=_parsed_data)
         return _heartbeat_with_value
 
     def _convert_to_heartbeat(self, heartbeat: Heartbeat, data: Any) -> Heartbeat:
+        # TODO: Maybe it could develop a package like mapstruct in kotlin.
         data: Dict[str, Any] = data
         heartbeat.datetime = data.get("datetime")
         return heartbeat
