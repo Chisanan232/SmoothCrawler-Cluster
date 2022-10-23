f64619804c493c9211fc0c0faeabd25ddac21821
“Chisanan232” <chi10211201@cycu.org.tw>
2022-10-23 20:01:45 +0800

    [New Feature] (code) 2 changes
    1. Finish Zookeeper operations implementation.
    
    2. Add crawler runner electing feature.

--- smoothcrawler_cluster/crawler.py
+++ smoothcrawler_cluster/crawler.py
@@ -1,9 +1,16 @@
-from typing import Type
+from datetime import datetime
+from typing import List, Type, TypeVar, Generic
 from abc import ABCMeta
+import time
 
+from .model.metadata_enum import CrawlerStateRole, TaskResult
 from .model.metadata import State, Task, Heartbeat
+from .election import BaseElection, IndexElection, ElectionResult
 from ._utils.converter import BaseConverter, JsonStrConverter
 from ._utils.zookeeper import ZookeeperClient
+
+
+BaseElectionType = TypeVar("BaseElectionType", bound=BaseElection)
 
 
 class BaseDistributedCrawler(metaclass=ABCMeta):
@@ -19,13 +26,18 @@ class ZookeeperCrawler(BaseDecentralizedCrawler):
     _Zookeeper_Client: ZookeeperClient = None
     __Default_Zookeeper_Hosts: str = "localhost:2181"
 
-    def __init__(self, runner: int, backup: int, name: str = "", zk_hosts: str = None, zk_converter: Type[BaseConverter] = None):
+    def __init__(self, runner: int, backup: int, name: str = "", index_sep: List[str] = ["-", "_"], zk_hosts: str = None,
+                 zk_converter: Type[BaseConverter] = None, election_strategy: Generic[BaseElectionType] = None):
         super().__init__()
+        self._total_crawler = runner + backup
         self._runner = runner
         self._backup = backup
+        self._crawler_role: CrawlerStateRole = None
+        self._index_sep = ""
 
         if name == "":
-            name = "crawler"
+            name = "sc-crawler_1"
+            self._index_sep = "_"
         self._crawler_name = name
 
         if zk_hosts is None:
@@ -35,6 +47,35 @@ class ZookeeperCrawler(BaseDecentralizedCrawler):
         if zk_converter is None:
             zk_converter = JsonStrConverter()
         self._zk_converter = zk_converter
+
+        if election_strategy is None:
+            election_strategy = IndexElection()
+        self._election_strategy = election_strategy
+        if self._index_sep == "":
+            for _sep in index_sep:
+                _crawler_name_list = self._crawler_name.split(sep=_sep)
+                if len(_crawler_name_list) > 1:
+                    self._index_sep = _sep
+                    self._crawler_index = _crawler_name_list[-1]
+                    self._election_strategy.identity = self._crawler_index
+                    break
+        else:
+            _crawler_name_list = self._crawler_name.split(sep=self._index_sep)
+            self._crawler_index = _crawler_name_list[-1]
+            self._election_strategy.identity = self._crawler_index
+
+        # TODO: It needs create another thread to keep updating heartbeat info to signal it's alive.
+        self.register()
+        if self.is_ready(interval=0.5, timeout=-1):
+            if self.elect() is ElectionResult.Winner:
+                self._crawler_role = CrawlerStateRole.Runner
+            else:
+                self._crawler_role = CrawlerStateRole.Backup_Runner
+            self._update_crawler_role(self._crawler_role)
+
+    @property
+    def role(self) -> CrawlerStateRole:
+        return self._crawler_role
 
     @property
     def zookeeper_hosts(self) -> str:
@@ -55,7 +96,22 @@ class ZookeeperCrawler(BaseDecentralizedCrawler):
             self._set_state_to_zookeeper(_state)
 
         # Register attribute of Task
+        if self._Zookeeper_Client.exist_node(path=self.task_zookeeper_path) is False:
+            _task = self._initial_task()
+            self._set_task_to_zookeeper(_task, create_node=True)
+        else:
+            _task = self._get_task_from_zookeeper()
+            _task = self._update_task(task=_task)
+            self._set_task_to_zookeeper(_task)
+
         # Register attribute of Heartbeat
+        if self._Zookeeper_Client.exist_node(path=self.heartbeat_zookeeper_path) is False:
+            _heartbeat = self._initial_heartbeat()
+            self._set_heartbeat_to_zookeeper(_heartbeat, create_node=True)
+        else:
+            _heartbeat = self._get_heartbeat_from_zookeeper()
+            _heartbeat = self._update_heartbeat(heartbeat=_heartbeat)
+            self._set_heartbeat_to_zookeeper(_heartbeat)
 
     @property
     def state_zookeeper_path(self) -> str:
@@ -107,51 +163,86 @@ class ZookeeperCrawler(BaseDecentralizedCrawler):
 
     def _initial_state(self) -> State:
         _state = State()
+        _state.total_crawler = self._runner + self._backup
         _state.total_runner = self._runner
         _state.total_backup = self._backup
-        _state.total_crawler = self._runner + self._backup
+        _state.role = CrawlerStateRole.Initial
+        _state.current_crawler = [self._crawler_name]
+        _state.current_runner = []
+        _state.current_backup = []
+        _state.standby_id = "0"
+        _state.fail_crawler = []
+        _state.fail_runner = []
+        _state.fail_backup = []
         return _state
 
     def _update_state(self, state: State) -> State:
+        state.total_crawler = self._runner + self._backup
         state.total_runner = self._runner
         state.total_backup = self._backup
-        state.total_crawler = self._runner + self._backup
+        state.current_crawler = state.current_crawler.append(self._crawler_name)
+        state.role = CrawlerStateRole.Initial
+        state.standby_id = "0"
         return state
+
+    def _update_crawler_role(self, role: CrawlerStateRole) -> None:
+        _state = self._get_state_from_zookeeper()
+        _state.role = role
+        if role is CrawlerStateRole.Runner:
+            _state.current_runner = _state.current_runner.append(self._crawler_name)
+        elif role is CrawlerStateRole.Backup_Runner:
+            _state.current_backup = _state.current_backup.append(self._crawler_name)
 
     def _initial_task(self) -> Task:
         _task = Task()
         # TODO: Consider about the task assigning timing
-        _task.task_result = None
+        _task.task_result = TaskResult.Nothing
         _task.task_content = None
         return _task
 
     def _update_task(self, task: Task) -> Task:
         # TODO: Consider about the task assigning timing
-        task.task_result = None
+        task.task_result = TaskResult.Nothing
         task.task_content = None
         return task
 
     def _initial_heartbeat(self) -> Heartbeat:
         _heartbeat = Heartbeat()
         # TODO: How to assign heartbeat value?
-        import datetime
-        _heartbeat.datetime = datetime.datetime.now()
+        _heartbeat.datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         return _heartbeat
 
     def _update_heartbeat(self, heartbeat: Heartbeat) -> Heartbeat:
         # TODO: How to assign heartbeat value?
-        import datetime
-        heartbeat.datetime = datetime.datetime.now()
+        heartbeat.datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         return heartbeat
 
-    def is_ready(self):
-        pass
+    def is_ready(self, interval: float = 0.5, timeout: float = -1) -> bool:
+        if timeout < -1:
+            raise ValueError("The option *timeout* value is incorrect. Please configure more than -1, and -1 means it never timeout.")
 
-    def elect(self):
-        pass
+        _start = time.time()
+        while True:
+            _state = self._get_state_from_zookeeper()
+            if _state.total_crawler == self._total_crawler:
+                return True
+            if timeout != -1:
+                if (time.time() - _start) >= timeout:
+                    return False
+            time.sleep(interval)
+
+    def elect(self) -> ElectionResult:
+        _state = self._get_state_from_zookeeper()
+        return self._election_strategy.elect(candidate=self._crawler_name, member=_state.current_crawler, index_sep=self._index_sep, spot=self._runner)
 
     def run(self):
-        pass
+        if self._crawler_role is CrawlerStateRole.Runner:
+            self.wait_for_task()
+            self.run_task()
+        elif self._crawler_role is CrawlerStateRole.Backup_Runner:
+            self.wait_and_standby()
+            self.discover()
+            self.activate()
 
     def wait_for_task(self):
         pass
