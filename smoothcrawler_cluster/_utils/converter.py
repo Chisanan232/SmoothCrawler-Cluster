0e4891559b03743fe6549dfea5f1111d75e88d3e
“Chisanan232” <chi10211201@cycu.org.tw>
2022-11-05 14:18:38 +0800

    [Refactor + Breaking Change] (code) 3 major changes:
    1. Move some functions up to super-class as template function.
    
    2. Define some abstract functions in super-class.
    
    3. Modify the implementation of function '_convert_to_heartbeat' with new data object *Heartbeat*.

--- smoothcrawler_cluster/_utils/converter.py
+++ smoothcrawler_cluster/_utils/converter.py
@@ -18,6 +18,46 @@ class BaseConverter(metaclass=ABCMeta):
     def deserialize(self, data: str) -> Any:
         pass
 
+    def state_to_str(self, state: State) -> str:
+        return self.serialize(data=self._convert_to_readable_object(obj=state))
+
+    def task_to_str(self, task: Task) -> str:
+        return self.serialize(data=self._convert_to_readable_object(obj=task))
+
+    def heartbeat_to_str(self, heartbeat: Heartbeat) -> str:
+        return self.serialize(data=self._convert_to_readable_object(obj=heartbeat))
+
+    @abstractmethod
+    def _convert_to_readable_object(self, obj: Generic[_BaseMetaDataType]) -> Any:
+        pass
+
+    def str_to_state(self, data: str) -> State:
+        _parsed_data: Dict[str, Any] = self.deserialize(data=data)
+        _state_with_value = self._convert_to_state(state=State(), data=_parsed_data)
+        return _state_with_value
+
+    @abstractmethod
+    def _convert_to_state(self, state: State, data: Any) -> State:
+        pass
+
+    def str_to_task(self, data: str) -> Task:
+        _parsed_data: Dict[str, Any] = self.deserialize(data=data)
+        _task_with_value = self._convert_to_task(task=Task(), data=_parsed_data)
+        return _task_with_value
+
+    @abstractmethod
+    def _convert_to_task(self, task: Task, data: Any) -> Task:
+        pass
+
+    def str_to_heartbeat(self, data: str) -> Heartbeat:
+        _parsed_data: Dict[str, Any] = self.deserialize(data=data)
+        _heartbeat_with_value = self._convert_to_heartbeat(heartbeat=Heartbeat(), data=_parsed_data)
+        return _heartbeat_with_value
+
+    @abstractmethod
+    def _convert_to_heartbeat(self, heartbeat: Heartbeat, data: Any) -> Heartbeat:
+        pass
+
 
 class JsonStrConverter(BaseConverter):
 
@@ -30,29 +70,10 @@ class JsonStrConverter(BaseConverter):
         _parsed_data: Dict[str, Any] = json.loads(data)
         return _parsed_data
 
-    def state_to_str(self, state: State) -> str:
-        # TODO: Move it to the super-class to be a template function
-        return self.serialize(data=self._convert_to_readable_object(obj=state))
-
-    def task_to_str(self, task: Task) -> str:
-        # TODO: Move it to the super-class to be a template function
-        return self.serialize(data=self._convert_to_readable_object(obj=task))
-
-    def heartbeat_to_str(self, heartbeat: Heartbeat) -> str:
-        # TODO: Move it to the super-class to be a template function
-        return self.serialize(data=self._convert_to_readable_object(obj=heartbeat))
-
     def _convert_to_readable_object(self, obj: Generic[_BaseMetaDataType]) -> Any:
         # TODO:
-        #  1. Set it to be a abstract method
-        #  2. Rename the function name to be more clear
+        #  1. Rename the function name to be more clear
         return obj.to_readable_object()
-
-    def str_to_state(self, data: str) -> State:
-        # TODO: Move it to the super-class to be a template function
-        _parsed_data: Dict[str, Any] = self.deserialize(data=data)
-        _state_with_value = self._convert_to_state(state=State(), data=_parsed_data)
-        return _state_with_value
 
     def _convert_to_state(self, state: State, data: Any) -> State:
         # TODO: Maybe it could develop a package like mapstruct in kotlin.
@@ -70,12 +91,6 @@ class JsonStrConverter(BaseConverter):
         state.fail_backup = data.get("fail_backup")
         return state
 
-    def str_to_task(self, data: str) -> Task:
-        # TODO: Move it to the super-class to be a template function
-        _parsed_data: Dict[str, Any] = self.deserialize(data=data)
-        _task_with_value = self._convert_to_task(task=Task(), data=_parsed_data)
-        return _task_with_value
-
     def _convert_to_task(self, task: Task, data: Any) -> Task:
         # TODO: Maybe it could develop a package like mapstruct in kotlin.
         data: Dict[str, Any] = data
@@ -83,14 +98,14 @@ class JsonStrConverter(BaseConverter):
         task.task_result = data.get("task_result")
         return task
 
-    def str_to_heartbeat(self, data: str) -> Heartbeat:
-        # TODO: Move it to the super-class to be a template function
-        _parsed_data: Dict[str, Any] = self.deserialize(data=data)
-        _heartbeat_with_value = self._convert_to_heartbeat(heartbeat=Heartbeat(), data=_parsed_data)
-        return _heartbeat_with_value
-
     def _convert_to_heartbeat(self, heartbeat: Heartbeat, data: Any) -> Heartbeat:
         # TODO: Maybe it could develop a package like mapstruct in kotlin.
         data: Dict[str, Any] = data
         heartbeat.heart_rhythm_time = data.get("heart_rhythm_time")
+        heartbeat.time_format = data.get("time_format")
+        heartbeat.update_time = data.get("update_time")
+        heartbeat.update_timeout = data.get("update_timeout")
+        heartbeat.heart_rhythm_timeout = data.get("heart_rhythm_timeout")
+        heartbeat.healthy_state = data.get("healthy_state")
+        heartbeat.task_state = data.get("task_state")
         return heartbeat
