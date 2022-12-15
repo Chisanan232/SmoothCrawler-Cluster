fc7cf827b07fd07ebc45b739bffbfd5c9abd66c3
“Chisanan232” <chi10211201@cycu.org.tw>
2022-12-15 22:52:55 +0800

    [Style] (code) Modify the line-long of content.

--- smoothcrawler_cluster/model/metadata_enum.py
+++ smoothcrawler_cluster/model/metadata_enum.py
@@ -1,3 +1,7 @@
+"""Module
+TODO: Add docstring
+"""
+
 from enum import Enum
 
 
@@ -18,9 +22,9 @@ class CrawlerStateRole(Enum):
     """
 
     Initial = "initial"
-    """ 
-    It only have this state in instantiating process of crawler and before runner election. And it would change this option 
-    to *Runner* or *Backup_Runner* after done runner election.
+    """
+    It only have this state in instantiating process of crawler and before runner election. And it would change this 
+    option to *Runner* or *Backup_Runner* after done runner election.
     """
 
     Runner = "runner"
@@ -28,18 +32,18 @@ class CrawlerStateRole(Enum):
 
     Backup_Runner = "backup-runner"
     """
-    **Backup Runner** role is the backup of **Runner**. It would activate (base on the setting, it may 
-    activate immediately) and run the web spider task if it still not finish. 
-    A **Backup Runner** would keep checking the heartbeat info of **Runner**, standby and ready to run 
-    in anytime for any one of **Runner** does not keep updating its own heartbeat info (and it would turn 
-    to **Dead Runner** at that time).
+    **Backup Runner** role is the backup of **Runner**. It would activate (base on the setting, it may activate 
+    immediately) and run the web spider task if it still not finish. 
+    A **Backup Runner** would keep checking the heartbeat info of **Runner**, standby and ready to run in anytime 
+    for any one of **Runner** does not keep updating its own heartbeat info (and it would turn to **Dead Runner** 
+    at that time).
     """
 
     Dead_Runner = "dead-runner"
-    """ 
-    If **Runner** cannot work finely, like the entire VM be shutdown where the crawler runtime environment 
-    in. It would turn to be **Dead Runner** from **Runner**. In other words, it must to be **Dead Runner** 
-    if it cannot keep updating its own heartbeat info.
+    """
+    If **Runner** cannot work finely, like the entire VM be shutdown where the crawler runtime environment in. It 
+    would turn to be **Dead Runner** from **Runner**. In other words, it must to be **Dead Runner** if it cannot 
+    keep updating its own heartbeat info.
     """
 
     Dead_Backup_Runner = "dead-backup-runner"
@@ -48,8 +52,8 @@ class CrawlerStateRole(Enum):
 
 class TaskResult(Enum):
     """
-    The task result means it is the result of running web spider task. The web spider task could classify to
-    be 4 different states: processing, done, terminate and error.
+    The task result means it is the result of running web spider task. The web spider task could classify to be
+    4 different states: processing, done, terminate and error.
     """
 
     Nothing = "nothing"
@@ -62,9 +66,9 @@ class TaskResult(Enum):
     """ Finish the task and it works finely without any exceptions. """
 
     Terminate = "terminate"
-    """ 
-    Web spider task running has been terminated so that it cannot finish all processes, but it doesn't occur 
-    any exceptions except KeyboardInterrupt.
+    """
+    Web spider task running has been terminated so that it cannot finish all processes, but it doesn't occur any 
+    exceptions except KeyboardInterrupt.
     """
 
     Error = "error"
@@ -77,24 +81,25 @@ class HeartState(Enum):
     """
 
     Newborn = "Newborn"
-    """ 
-    The initialized heartbeat state of a crawler instance which just be created, in other words, crawler instance's heartbeat 
-    state would be this before it run updating heartbeat process. 
+    """
+    The initialized heartbeat state of a crawler instance which just be created, in other words, crawler instance's 
+    heartbeat state would be this before it run updating heartbeat process. 
     """
 
     Healthy = "Healthy"
     """ This means that current crawler instance runs finely without any issue. """
 
     Arrhythmia = "Arrhythmia"
-    """ 
-    It means that the current crawler instance is late to update property *heart_rhythm_time*. It likes its heartbeat rate 
-    is not stable and cannot work normally lead to it looks or sounds not good, just like arrhythmia in human.
+    """
+    It means that the current crawler instance is late to update property *heart_rhythm_time*. It likes its heartbeat 
+    rate is not stable and cannot work normally lead to it looks or sounds not good, just like arrhythmia in human.
     """
 
     Asystole = "Asystole"
-    """ 
-    If a crawler instance's property *healthy_state* become this state, it means the crawler be judged it's dead by other 
-    alive crawler instances. So the dead crawler instance won't run anymore and list to *fail_xxx* property in *State*.
+    """
+    If a crawler instance's property *healthy_state* become this state, it means the crawler be judged it's dead by 
+    other alive crawler instances. So the dead crawler instance won't run anymore and list to *fail_xxx* property in 
+    *State*.
     """
 
     ApparentDeath = "Apparent Death"
