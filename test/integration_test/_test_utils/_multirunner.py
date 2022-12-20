fd2a89ed4cd31db481d83463398e2f94ea7b91d3
“Chisanan232” <chi10211201@cycu.org.tw>
2022-12-20 08:59:55 +0800

    [Style] (test) Fix Pylint style issues of 'invalid-name / C0103', 'line-too-long / C0301'.

--- test/integration_test/_test_utils/_multirunner.py
+++ test/integration_test/_test_utils/_multirunner.py
@@ -3,19 +3,24 @@ import multiprocessing as mp
 import threading
 
 
-def run_multi_processes(processes_num: int, target_function, index_sep_char: str = "_", daemon: bool = True) -> List[mp.Process]:
+def run_multi_processes(
+        processes_num: int,
+        target_function,
+        index_sep_char: str = "_",
+        daemon: bool = True,
+) -> List[mp.Process]:
     processes = []
     for i in range(1, processes_num + 1):
-        _crawler_process = mp.Process(target=target_function, args=(f"sc-crawler{index_sep_char}{i}",))
-        _crawler_process.daemon = daemon
-        processes.append(_crawler_process)
+        crawler_process = mp.Process(target=target_function, args=(f"sc-crawler{index_sep_char}{i}",))
+        crawler_process.daemon = daemon
+        processes.append(crawler_process)
 
-    for _process in processes:
-        _process.start()
+    for process in processes:
+        process.start()
 
     if daemon is True:
-        for _process in processes:
-            _process.join()
+        for process in processes:
+            process.join()
 
     return processes
 
@@ -26,24 +31,24 @@ def run_2_diff_workers(func1_ps: tuple, func2_ps: tuple, worker: str = "thread")
 
     workers = []
     if worker == "thread":
-        _func1_worker = threading.Thread(target=func1, args=(func1_args or ()))
-        _func2_worker = threading.Thread(target=func2, args=(func2_args or ()))
+        func1_worker = threading.Thread(target=func1, args=(func1_args or ()))
+        func2_worker = threading.Thread(target=func2, args=(func2_args or ()))
     elif worker == "process":
-        _func1_worker = mp.Process(target=func1, args=(func1_args or ()))
-        _func2_worker = mp.Process(target=func2, args=(func2_args or ()))
+        func1_worker = mp.Process(target=func1, args=(func1_args or ()))
+        func2_worker = mp.Process(target=func2, args=(func2_args or ()))
     else:
         raise ValueError(f"Doesn't support worker type {worker}.")
-    _func1_worker.daemon = func1_daemon
-    _func2_worker.daemon = func2_daemon
-    workers.append(_func1_worker)
-    workers.append(_func2_worker)
+    func1_worker.daemon = func1_daemon
+    func2_worker.daemon = func2_daemon
+    workers.append(func1_worker)
+    workers.append(func2_worker)
 
-    _func1_worker.start()
-    _func2_worker.start()
+    func1_worker.start()
+    func2_worker.start()
 
     if func1_daemon is False:
-        _func1_worker.join()
+        func1_worker.join()
     if func2_daemon is False:
-        _func2_worker.join()
+        func2_worker.join()
 
     return workers
