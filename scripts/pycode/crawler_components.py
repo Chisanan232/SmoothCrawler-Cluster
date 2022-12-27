31e84cf8e88d958f27c6d484716d8a4470efe8cb
“Chisanan232” <chi10211201@cycu.org.tw>
2022-12-27 09:46:40 +0800

    [Breaking Change] (code) Remove unused code.

--- scripts/pycode/crawler_components.py
+++ scripts/pycode/crawler_components.py
@@ -3,16 +3,6 @@ from smoothcrawler.components.httpio import HTTP
 from smoothcrawler.components.data import BaseHTTPResponseParser, BaseDataHandler
 from typing import Any
 import requests
-import os
-
-
-# Test_Example_URL = "http://www.example.com/"
-_CRAWLER_MODE = os.getenv("CRAWLER_MODE", "general")
-TEST_EXAMPLE_URL = os.getenv("TEST_URL", "http://www.example.com/")
-_RUNNER_CRAWLER_VALUE = int(os.getenv("CLUSTER_RUNNER", 2))
-_BACKUP_CRAWLER_VALUE = int(os.getenv("CLUSTER_BACKUP", 1))
-_CRAWLER_NAME = os.getenv("CRAWLER_NAME", "sc-crawler_1")
-_ZK_HOSTS = os.getenv("ZK_HOSTS", "localhost:2181")
 
 
 class RequestsHTTPRequest(HTTP):
