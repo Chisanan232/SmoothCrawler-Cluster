79bc074ddf8364d4e87924d50e8faad30c6838de
“Chisanan232” <chi10211201@cycu.org.tw>
2023-01-25 14:36:14 +0800

    [Refactor] (test) Move the unit-test about Zookeeper path to '_utils.zookeeper' from 'crawler'.

--- test/unit_test/_utils/zookeeper.py
+++ test/unit_test/_utils/zookeeper.py
@@ -1,11 +1,51 @@
 import pytest
 
-from smoothcrawler_cluster._utils.zookeeper import ZookeeperNode
+from smoothcrawler_cluster._utils.zookeeper import ZookeeperNode, ZookeeperPath
 
-from ..._values import Test_Zookeeper_Path, Test_Zookeeper_String_Value
+from ..._assertion import ValueFormatAssertion
+from ..._values import (
+    Test_Zookeeper_Path,
+    Test_Zookeeper_String_Value,
+    _Crawler_Group_Name_Value,
+    _Crawler_Name_Value,
+)
 
 
 class TestZookeeperPath:
+    @pytest.fixture(scope="function")
+    def zk_path(self) -> ZookeeperPath:
+        return ZookeeperPath(name=_Crawler_Name_Value, group=_Crawler_Group_Name_Value)
+
+    def test_property_group_state_zookeeper_path(self, zk_path: ZookeeperPath):
+        # Get value by target method for testing
+        path = zk_path.group_state_zookeeper_path
+
+        # Verify values
+        ValueFormatAssertion(target=path, regex=r"smoothcrawler/group/[\w\-_]{1,64}/state")
+
+    def test_property_node_state_zookeeper_path(self, zk_path: ZookeeperPath):
+        # Get value by target method for testing
+        path = zk_path.node_state_zookeeper_path
+
+        # Verify values
+        ValueFormatAssertion(target=path, regex=r"smoothcrawler/node/[\w\-_]{1,64}[-_]{1}[0-9]{1,10000}/state")
+
+    def test_property_task_zookeeper_path(self, zk_path: ZookeeperPath):
+        # Get value by target method for testing
+        path = zk_path.task_zookeeper_path
+
+        # Verify values
+        ValueFormatAssertion(target=path, regex=r"smoothcrawler/node/[\w\-_]{1,64}[-_]{1}[0-9]{1,10000}/task")
+
+    def test_property_heartbeat_zookeeper_path(self, zk_path: ZookeeperPath):
+        # Get value by target method for testing
+        path = zk_path.heartbeat_zookeeper_path
+
+        # Verify values
+        ValueFormatAssertion(target=path, regex=r"smoothcrawler/node/[\w\-_]{1,64}[-_]{1}[0-9]{1,10000}/heartbeat")
+
+
+class TestZookeeperNode:
     @pytest.fixture(scope="function")
     def zk_path(self) -> ZookeeperNode:
         return ZookeeperNode()
