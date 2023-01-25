7e53a83771ab29d578111dcb9817db8ac8963f7a
“Chisanan232” <chi10211201@cycu.org.tw>
2023-01-25 14:47:49 +0800

    [Refactor] (test) Rename unit test item.

--- test/unit_test/_utils/zookeeper.py
+++ test/unit_test/_utils/zookeeper.py
@@ -16,28 +16,28 @@ class TestZookeeperPath:
     def zk_path(self) -> ZookeeperPath:
         return ZookeeperPath(name=_Crawler_Name_Value, group=_Crawler_Group_Name_Value)
 
-    def test_property_group_state_zookeeper_path(self, zk_path: ZookeeperPath):
+    def test_property_group_state(self, zk_path: ZookeeperPath):
         # Get value by target method for testing
         path = zk_path.group_state
 
         # Verify values
         ValueFormatAssertion(target=path, regex=r"smoothcrawler/group/[\w\-_]{1,64}/state")
 
-    def test_property_node_state_zookeeper_path(self, zk_path: ZookeeperPath):
+    def test_property_node_state(self, zk_path: ZookeeperPath):
         # Get value by target method for testing
         path = zk_path.node_state
 
         # Verify values
         ValueFormatAssertion(target=path, regex=r"smoothcrawler/node/[\w\-_]{1,64}[-_]{1}[0-9]{1,10000}/state")
 
-    def test_property_task_zookeeper_path(self, zk_path: ZookeeperPath):
+    def test_property_task(self, zk_path: ZookeeperPath):
         # Get value by target method for testing
         path = zk_path.task
 
         # Verify values
         ValueFormatAssertion(target=path, regex=r"smoothcrawler/node/[\w\-_]{1,64}[-_]{1}[0-9]{1,10000}/task")
 
-    def test_property_heartbeat_zookeeper_path(self, zk_path: ZookeeperPath):
+    def test_property_heartbeat(self, zk_path: ZookeeperPath):
         # Get value by target method for testing
         path = zk_path.heartbeat
 
