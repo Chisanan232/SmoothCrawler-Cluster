53481122b16804f77b1ecd2949e7a4ec3eca7b8f
“Chisanan232” <chi10211201@cycu.org.tw>
2023-02-02 14:12:32 +0800

    [Breaking Change] (script) Add testing sub-package 'crawler' for integration test of CI shell script.

--- scripts/ci/get-integration-test-paths.sh
+++ scripts/ci/get-integration-test-paths.sh
@@ -5,6 +5,7 @@ runtime_os=$1
 
 declare -a init_tests
 declare -a utils_tests
+declare -a crawler_tests
 
 getalltests() {
     # shellcheck disable=SC2207
@@ -21,6 +22,11 @@ getalltests() {
         # shellcheck disable=SC2124
         # shellcheck disable=SC2178
         utils_tests=${alltestpaths[@]}
+    elif echo "$1" | grep -q "crawler";
+    then
+        # shellcheck disable=SC2124
+        # shellcheck disable=SC2178
+        crawler_tests=${alltestpaths[@]}
     else
         # shellcheck disable=SC2124
         # shellcheck disable=SC2178
@@ -30,11 +36,13 @@ getalltests() {
 
 init_path=./test/integration_test/
 utils_path=./test/integration_test/_utils/
+crawler_path=./test/integration_test/crawler/
 
 getalltests $init_path
 getalltests $utils_path
+getalltests $crawler_path
 
-dest=( "${init_tests[@]} ${utils_tests[@]}" )
+dest=( "${init_tests[@]} ${utils_tests[@]} ${crawler_tests[@]}" )
 
 if echo "$runtime_os" | grep -q "windows";
 then
