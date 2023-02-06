8d4049a82901d0fccc3f7ddcca07e6ae908502f2
“Chisanan232” <chi10211201@cycu.org.tw>
2023-02-06 16:15:03 +0800

    [Docs] (code) Add class level docstring.

--- smoothcrawler_cluster/exceptions.py
+++ smoothcrawler_cluster/exceptions.py
@@ -5,7 +5,15 @@ Some exceptions or errors for this package *SmoothCrawler-Cluster*'s usage.
 
 
 class CrawlerIsDeadError(RuntimeError):
+    """This error for module *dispatcher*. Dispatcher object would raise this error if dead role calls it."""
+
     def __init__(self, crawler_name: str, group: str):
+        """
+
+        Args:
+            crawler_name (str): The current crawler instance's name.
+            group (str): The group which current crawler instance is in.
+        """
         self._crawler_name = crawler_name
         self._group = group
 
@@ -14,5 +22,7 @@ class CrawlerIsDeadError(RuntimeError):
 
 
 class StopUpdateHeartbeat(BaseException):
+    """This error for module *workflow*. The signal of stopping updating heartbeat."""
+
     def __str__(self):
         return "Stop updating heartbeat now."
