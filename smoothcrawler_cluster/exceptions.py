02955d878f7786cbf97904343d9ed0c5fbd3f401
“Chisanan232” <chi10211201@cycu.org.tw>
2023-02-06 15:39:54 +0800

    [Docs] (code) Add module level docstring.

--- smoothcrawler_cluster/exceptions.py
+++ smoothcrawler_cluster/exceptions.py
@@ -1,3 +1,9 @@
+"""*Global exception module*
+
+Some exceptions or errors for this package *SmoothCrawler-Cluster*'s usage.
+"""
+
+
 class StopUpdateHeartbeat(BaseException):
     def __str__(self):
         return "Stop updating heartbeat now."
