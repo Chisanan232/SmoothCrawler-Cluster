5eb609fd0dfbef8fd9cad8928bf0a8ef0b348d58
“Chisanan232” <chi10211201@cycu.org.tw>
2023-02-06 15:38:40 +0800

    [Style] (code) Remove unused code.

--- smoothcrawler_cluster/exceptions.py
+++ smoothcrawler_cluster/exceptions.py
@@ -1,15 +1,3 @@
-"""
-Global exceptions module
-
-Some customized exceptions for this package SmoothCrawler-Cluster.
-"""
-
-
-class ZookeeperCrawlerNotReady(RuntimeError):
-    def __str__(self):
-        return "Current crawler instance is not ready for running. Its *current_runner* still be empty."
-
-
 class StopUpdateHeartbeat(BaseException):
     def __str__(self):
         return "Stop updating heartbeat now."
