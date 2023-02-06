021fdb62a01d8953622fca94b7430b6b1de30997
“Chisanan232” <chi10211201@cycu.org.tw>
2023-02-06 16:09:56 +0800

    [New Feature] (code) Add new error about crawler instance is dead.

--- smoothcrawler_cluster/exceptions.py
+++ smoothcrawler_cluster/exceptions.py
@@ -4,6 +4,15 @@ Some exceptions or errors for this package *SmoothCrawler-Cluster*'s usage.
 """
 
 
+class CrawlerIsDeadError(RuntimeError):
+    def __init__(self, crawler_name: str, group: str):
+        self._crawler_name = crawler_name
+        self._group = group
+
+    def __str__(self):
+        return f"Current crawler instance '{self._crawler_name}' in group '{self._group}' is dead."
+
+
 class StopUpdateHeartbeat(BaseException):
     def __str__(self):
         return "Stop updating heartbeat now."
